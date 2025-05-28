354060eb8ace5862ab12004067ac3c000a1f4e76
simplyObare <josephatowiny@gmail.com>
2025-05-28 17:15:51 +0300

    developing models, making migrations and creating superuser

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} by {self.author.username}"
